44dd3f68bd79a68df60d6e328bdb310fa6985eb0
ankita-parashar15 <sharma11aniket@gmail.com>
2025-12-04 21:44:03 +0530

    updated end point

--- src/lib/authInstances.ts
+++ src/lib/authInstances.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie';
 
 // const API_BASE_URL = "http://localhost:3000/api";
-const API_BASE_URL = "https://worklah-updated-dec.onrender.com";
+const API_BASE_URL = "https://worklah-updated-dec.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
